6287c4fb7356e73864792f711ac5a747b99d02b2
fullStack-devmax <heymywork01@gmail.com>
2025-08-30 21:30:09 +0500

    installed required modules, converted home page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+export default function About() {
+    return (
+        <h2>About page</h2>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "../../components/Hero";
+// import MockIelts from "../../components/Hero";
+
+export default function Home() {
+  console.log("page working");
+  
+  return (
+    <div className="min-h-screen bg-background">
+      <main className="p-0">
+        <Hero />
+        {/* <MockIelts /> */}
+      </main>
+    </div>
+  );
+}
